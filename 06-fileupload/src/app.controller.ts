599eb452b8d1d6b968a8be355d193083eb6fb583
Sonissapkota <sonishsapkota47@gmail.com>
2025-01-19 17:45:49 +0545

    Made some changes

--- 06-fileupload/src/app.controller.ts
+++ 06-fileupload/src/app.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { AppService } from "./app.service";
-import { diskStorage } from "multer";
-import { extname } from "path";
 
 @Controller("user")
 export class AppController{
@@ -16,7 +14,6 @@ export class AppController{
     @Post("upload")
     @UseInterceptors(FileInterceptor('file', ))
     uploadfile(@UploadedFile() file: Express.Multer.File){
-        console.log('file', file);
         return "File uplaod section"
     }
 }
